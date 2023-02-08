b91f09267a013fec472192da4c823491c72d3047
Viral26 <104312448+Viral26@users.noreply.github.com>
2023-02-08 23:59:47 +0530

    Update settings.py

--- DelayAnalysis/settings.py
+++ DelayAnalysis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8(_l_0gk9(hl^#h-@r$+z7es!0kojuyo2a+(+b_i(&v_1t7@#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
